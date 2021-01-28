7b010942e81b4d1e55c6e07a6e7f4e21a157a1b2
Spencer <>
2021-01-28 15:41:39 -0500

    Add at command for scheduling jobs

--- at.py
+++ at.py
@@ -1,5 +1,63 @@
 import argparse
+import datetime
+import glob
+import os
+import signal
+import sqlite3
+import sys
 
-from atd.py import
+from atd import DB_PATH
 
-if __name__ == '__main':
+ATD_SENTINAL = b'atd.py'
+
+class ProcNotFound(Exception):
+    pass
+
+def write_job_record(conn, command, ts):
+    c = conn.cursor()
+    c.execute('''
+    INSERT INTO jobs
+    (command, timestamp, was_run)
+    VALUES
+    (?, ?, false)
+    ''', (command, ts))
+    conn.commit()
+
+
+def parse_time(time_str):
+    # TODO: accept more formats
+    dt = datetime.datetime.fromisoformat(time_str)
+    if dt < datetime.datetime.now(tz=datetime.timezone.utc):
+        raise ValueError("Scheduled time must be in the future")
+    return int(dt.timestamp())
+
+
+# TODO: is there a better way? Maybe have atd process write a pidfile?
+def find_pid(sentinal):
+    for proc_file in glob.glob('/proc/*/cmdline'):
+        with open(proc_file, 'rb') as f:
+            data = f.read()
+        if sentinal in data:
+            return int(proc_file.split('/')[2])
+    raise ProcNotFound
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('time')
+    args = parser.parse_args()
+    cmd = sys.stdin.read()
+
+    parsed_time = parse_time(args.time)
+
+    try:
+        atd_pid = find_pid(ATD_SENTINAL)
+    except ProcNotFound:
+        print("Couldn't find atd process")
+        sys.exit(1)
+
+    conn = sqlite3.connect(DB_PATH)
+    write_job_record(conn, cmd, parsed_time)
+
+    # notify atd
+    os.kill(atd_pid, signal.SIGUSR1)
+    print('Scheduled {} to run at {}'.format(cmd, args.time))
